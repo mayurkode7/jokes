da3cde3821f31d042bf9b613f4e6fe2c7a90bbb3
Mayur Kode <mayurkode7@gmail.com>
2021-04-28 20:14:14 +0530

    install dependencies, add header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      Jokes
-    </div>
+    <section>
+
+      <Header />
+      <main>
+      </main>
+      <Footer />
+
+    </section>
   );
 }
 
---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            Footer be here
+        </div>
+    )
+}
+
+export default Footer
