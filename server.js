4bda992f355620b973fd6fc0a37b50286741fa85
Mayur Kode <mayurkode7@gmail.com>
2021-05-11 21:14:22 +0530

    init app, define basic path and listen on port

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.port || 5000
+
+
+// define basic path
+app.get('/', (req, res) => {
+    res.send('Hello Server')
+});
+
+
+// listen to port
+app.listen(PORT, () => {
+    console.log(`Server started on port`);
+});
